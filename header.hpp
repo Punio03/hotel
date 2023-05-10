69628a8431c71d2f3faf275426369d0fb827d239
Jakub Malczak <118765990+Punio03@users.noreply.github.com>
2023-05-10 15:00:17 +0200

    początek projektu

---
+++ header.hpp
@@ -0,0 +1,19 @@
+#ifndef UNTITLED1_HEADER_HPP
+#define UNTITLED1_HEADER_HPP
+#include <iostream>
+
+class Hotel {};
+class Pokoj {};
+class Klient {};
+class Rezerwacja {};
+class SystemRezerwacji {};
+class Aplikacja {};
+class Administrator {};
+class ObluslugaPokoi {};
+class Rachunek {};
+class Restauracja {};
+class Opinia {};
+class Pakiet {};
+class Statystyki {};
+
+#endif //UNTITLED1_HEADER_HPP
