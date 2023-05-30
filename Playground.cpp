49c954809382fea9dd6b62f4d078ac5f17f51a63
PiotrS <117858696+PiotrStachowicz@users.noreply.github.com>
2023-05-30 22:49:15 +0200

    Rozszerzyłem Hotel

--- Playground.cpp
+++ Playground.cpp
@@ -1,6 +1,9 @@
 #include "header.hpp"
 int main(void){
-    Room p1(12,3,320);
-    cout << p1 << endl;
+    vector<Room> vec;
+    for(int i = 0;i<100;i++){
+        Room temp(i,3,320);
+        cout << temp << endl;
+    }
     return 0;
 }
--- header.hpp
+++ header.hpp
@@ -21,7 +21,10 @@ public:
     inline void setRoomID(int newRoomID)  noexcept { this->RoomID = newRoomID; }
     inline void setCapacity(int newCapacity)  noexcept { this->Capacity = newCapacity; }
     inline void setPrice(int newPrice)  noexcept { this->Price = newPrice; }
-    inline friend ostream& operator<<(ostream& out, const Room &room) noexcept { out << "ID:" << room.RoomID << endl << "Capacity:" << room.Capacity << endl<< "Price for night:" << room.Price << "$" << endl << "Availability:" << room.Available; return out; }
+    inline friend ostream& operator<<(ostream& out, const Room &room) noexcept {
+        out << "ID:" << room.RoomID << endl << "Capacity:" << room.Capacity << endl<< "Price for night:" << room.Price << "$" << endl << "Availability:" << room.Available;
+        return out;
+    }
 }; // gotowe
 class Address final{
 public:
@@ -30,23 +33,35 @@ public:
     string ZipCode;
     string City;
     string TelephoneNumber;
-    inline Address(string &newCountry, string &newStreetName, string &newZipCode, string &newCity, string &newTelephoneNumber) noexcept : Country(newCountry), StreetName(newStreetName), ZipCode(newZipCode), City(newCity), TelephoneNumber(newTelephoneNumber) { }
+    inline Address(string newCountry, string newStreetName, string newZipCode, string newCity, string newTelephoneNumber) noexcept : Country(newCountry), StreetName(newStreetName), ZipCode(newZipCode), City(newCity), TelephoneNumber(newTelephoneNumber) { }
     inline Address(Address const  &adress) = default;
     inline Address(Address &&adress) = default;
     inline ~Address() = default;
-    inline vector<string> AdressInfo() { return {this->Country,this->StreetName, this->ZipCode, this->City, this->TelephoneNumber}; }
-    inline friend ostream& operator<<(ostream& out, const Address &address) noexcept { out << "Country:" << address.Country << endl << "StreetName:" << address.StreetName << endl << "ZipCode:" << address.ZipCode << endl << "City:" << address.City << endl << "Telephone Number:" << address.TelephoneNumber << endl; return out; }
+    inline vector<string> AdressInfo() { return { this->Country,this->StreetName, this->ZipCode, this->City, this->TelephoneNumber }; }
+    inline friend ostream& operator<<(ostream& out, const Address &address) noexcept {
+        out << "Country:" << address.Country << endl << "StreetName:" << address.StreetName << endl << "ZipCode:" << address.ZipCode << endl << "City:" << address.City << endl << "Telephone Number:" << address.TelephoneNumber << endl;
+        return out;
+    }
     inline friend bool operator==(Address &address1, Address &address2) { return address1.AdressInfo() == address2.AdressInfo(); }
 }; // gotowe
+class Opinion {
+
+};
+class Restauration {
+
+};
 class Hotel {
 private:
     string Name;
     Address HotelAddress;
     vector<Room> Rooms;
+    vector<Opinion> Opinions;
+    Restauration HotelRestauration;
 public:
-    inline Hotel(string &newName, Address &newAddress, vector<Room>&newRooms) noexcept : Name(newName), HotelAddress(newAddress), Rooms(newRooms) { }
+    inline Hotel(string newName, Address newAddress, vector<Room>newRooms, vector<Opinion>newOpinions, Restauration newRestauration) noexcept : Name(newName), HotelAddress(newAddress), Rooms(newRooms), Opinions(newOpinions), HotelRestauration(newRestauration) { }
     inline void AddRoom(const Room &newRoom){ this->Rooms.push_back(newRoom); }
-    //...
+    inline int SearchForRoom(int RoomID) { for(int i = 0;i<Rooms.size();i++){ if(Rooms[i].getRoomID()==RoomID){ return i; } } return -1; }
+    inline friend ostream& operator<<(ostream& out, const Hotel &hotel) { for(auto room : hotel.Rooms){ out << room << endl; } return out; }
 };
 class Client {};
 class Reservation {};
@@ -55,8 +70,6 @@ class Application {};
 class Administrator {};
 class Invoice {};
 class Order {};
-class Restauration {};
-class Opinion {};
 class Menu {};
 class Date {};
 class Services {};
