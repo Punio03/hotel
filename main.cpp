43876fa53b10c36fd8c4c78eb54571c65771b64a
jakubmalczak <338401@uwr.edu.pl>
2023-06-15 17:22:31 +0200

    Wszystko, ale do poprawy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "header.hpp"
 
 int main() {
-    Menu fef{{{"olej",157}, {"kaszanka", 162}}};
-    cout << fef;
+    Client klient1{"Jakub", "Malczak", 0, new Invoice{}};
+    Administrator admin = {};
+    Room room1{1,1,100};
+    Room room2{2,2,200};
+    Restaurant restaurant1{};
+    Services services1{};
+    Hotel *hotel1 = new Hotel{"Neptune",Address{"Poland","Wojna","47-201","Gdansk","424242"},restaurant1,services1};
+    hotel1->addRoom(room1);
+    hotel1->addRoom(room2);
+    cout << *hotel1;
+    cout << klient1 << endl;
+    ReservationSystem rs{};
+    rs.addHotel(hotel1);
+    DateTime checkin{2023,12,12};
+    rs.reservateRoom(1,"Neptune",klient1,checkin,checkin);
+    cout << klient1 << endl;
+    cout << *hotel1;
 }
